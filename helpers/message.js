742afc5041c03abb5078afb6bac7be750857556e
YhomiAce <kareemyomi91@gmail.com>
2022-10-16 21:40:05 +0100

    fix return error

--- helpers/message.js
+++ helpers/message.js
@@ -394,7 +394,7 @@ const mobileVerifyMessage = (name, token) => {
   <td class="es-m-txt-l" bgcolor="#ffffff" align="left" style="Margin:0;padding-top:20px;padding-bottom:20px;padding-left:30px;padding-right:30px"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:lato, 'helvetica neue', helvetica, arial, sans-serif;line-height:27px;color:#666666;font-size:18px">Hi, <strong>${name}</strong><br>We're excited to have you get started. First, you need to activate&nbsp;your account. Enter the activation token on the mobile app.</p></td>
   </tr>
   <tr style="border-collapse:collapse">
-  <td align="center" style="Margin:0;padding-left:10px;padding-right:10px;padding-top:35px;padding-bottom:35px"><span class="es-button-border" style="border-style:solid;border-color:#3F79AD;background:1px;border-width:1px;display:inline-block;border-radius:2px;width:auto">${token}</span></td>
+  <td align="center" style="Margin:0;padding-left:10px;padding-right:10px;padding-top:35px;padding-bottom:35px"><span class="es-button-border" style="border-style:solid;border-color:#3F79AD;background:1px;border-width:1px;display:inline-block;border-radius:2px;width:auto; padding: 20px;"><h1>${token}</h1></span></td>
   </tr>
 
 
