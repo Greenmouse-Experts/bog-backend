546814f26c8ca05c45a78223562a31d83d8669d6
YhomiAce <kareemyomi91@gmail.com>
2022-12-08 23:35:43 +0100

    changes

--- controllers/ProductContoller.js
+++ controllers/ProductContoller.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable no-await-in-loop */
 /* eslint-disable no-unused-vars */
 require("dotenv").config();
--- controllers/ProjectController.js
+++ controllers/ProjectController.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-param-reassign */
 require("dotenv").config();
