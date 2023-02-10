90caaf48ff4c1bd3b2dbb3b90eaea9450ecbcb9d
YhomiAce <kareemyomi91@gmail.com>
2023-02-10 01:49:47 +0100

    add subscription to transactions

--- routes/transactions.js
+++ routes/transactions.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 const Auth = require("../middleware/auth");
-const Access = require("../middleware/access");
+// const Access = require("../middleware/access");
 
 const Transaction = require("../helpers/transactions");
 
