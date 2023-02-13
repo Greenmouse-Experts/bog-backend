ea9d3014f68f000a13718884788b19f1c8a4e917
YhomiAce <kareemyomi91@gmail.com>
2023-02-14 00:39:17 +0100

    dispatch project

--- routes/notification.js
+++ routes/notification.js
@@ -1,18 +1,21 @@
 const express = require("express");
 
 const router = express.Router();
-const Auth= require("../middleware/auth");
+const Auth = require("../middleware/auth");
 const Access = require("../middleware/access");
 
 const NotificationController = require("../controllers/NotificationController");
 
 router
   .route("/notifications/admin")
-  .get( [Auth, Access.verifyAccess], NotificationController.getAllAdminNotifications);
+  .get(
+    [Auth, Access.verifyAccess],
+    NotificationController.getAllAdminNotifications
+  );
 
 router
   .route("/notifications/user/:userId")
-  .get( [Auth, Access.verifyAccess], NotificationController.getAllAUserNotifications);
+  .get(Auth, NotificationController.getAllAUserNotifications);
 
 router
   .route("/notifications/mark-read/:notificationId")
