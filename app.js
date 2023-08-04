32b683d4fb75a488a689dc52f68065db83bfd59b
Akinbebije Micheal <michealakinbebije@gmail.com>
2023-08-04 08:11:56 +0100

    gggd

--- app.js
+++ app.js
@@ -47,16 +47,16 @@ app.use(express.static(path.join(__dirname, "uploads")));
 
 // //log file
 
-var fs = require("fs");
-var util = require("util");
-var log_file = fs.createWriteStream(__dirname + "/debug.log", { flags: "w" });
-var log_stdout = process.stdout;
+// var fs = require("fs");
+// var util = require("util");
+// var log_file = fs.createWriteStream(__dirname + "/debug.log", { flags: "w" });
+// var log_stdout = process.stdout;
 
-console.log = function(d) {
-  //
-  log_file.write(util.format(d) + "\n");
-  log_stdout.write(util.format(d) + "\n");
-};
+// console.log = function(d) {
+//   //
+//   log_file.write(util.format(d) + "\n");
+//   log_stdout.write(util.format(d) + "\n");
+// };
 
 // app.use(session({
 //   secret: 'keyboard cat',
