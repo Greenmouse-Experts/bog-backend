7b0640a1eb72c31b66fb93bdeff05cda7dfa6fcf
manuelExtra <olaleyeemmanuel23@gmail.com>
2023-09-12 13:17:22 +0100

    service mailer correction

--- service/attachmentEmailService.js
+++ service/attachmentEmailService.js
@@ -1,14 +1,40 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
 
+const email_data = {
+  // APP_NAME: "BOG",
+  // EMAIL_PASSWORD: "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",
+  // EMAIL_USERNAME: "apikey",
+  // EMAIL_HOST: "smtp.sendgrid.net",
+  // EMAIL_PORT: 465,
+  // EMAIL_FROM: "buildonthego2023@gmail.com",
+  // EMAIL_SECURE: true
+  APP_NAME: "BOG",
+  EMAIL_PASSWORD: "VK!x_6%vo___",
+  EMAIL_USERNAME: "no-reply@buildonthego.com",
+  EMAIL_HOST: "mail.buildonthego.com",
+  EMAIL_PORT: 587,
+  EMAIL_FROM: "no-reply@buildonthego.com",
+  EMAIL_SECURE: false,
+  // EMAIL_PASSWORD: "Ca2Y5tf1;D^v",
+  // EMAIL_USERNAME: "no-reply@buildonthego.com",
+  // EMAIL_HOST: "mail.buildonthego.com",
+  // EMAIL_PORT: 587,
+  // EMAIL_FROM: "no-reply@buildonthego.com",
+  // EMAIL_SECURE: true,
+};
+
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: process.env.EMAIL_SECURE, // true for 465, false for other ports
+  host: email_data.EMAIL_HOST,
+  port: email_data.EMAIL_PORT,
+  secure: email_data.EMAIL_SECURE || false, // true for 465, false for other ports
   auth: {
-    user: process.env.EMAIL_USERNAME, // generated ethereal user
-    pass: process.env.EMAIL_PASSWORD // generated ethereal password
-  }
+    user: email_data.EMAIL_USERNAME, // generated ethereal user
+    pass: email_data.EMAIL_PASSWORD, // generated ethereal password
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 exports.sendMail = async (email, message, subject, files = []) => {
