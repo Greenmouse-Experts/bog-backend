ec02673007db4fed2bbbb6d82705926a67f84127
manuelExtra <olaleyeemmanuel23@gmail.com>
2024-03-19 16:32:47 +0100

    sync

--- routes/address.js
+++ routes/address.js
@@ -1,34 +1,39 @@
-const express = require("express");
-
+const express = require('express');
 
 const router = express.Router();
-const Auth = require("../middleware/auth");
-const Access = require("../middleware/access");
+const Auth = require('../middleware/auth');
+const Access = require('../middleware/access');
 
-const DeliveryAddressController = require("../controllers/DeliveryAddressController");
+const DeliveryAddressController = require('../controllers/DeliveryAddressController');
 
-const { validate, addressValidation } = require("../helpers/validators");
+const { validate, addressValidation } = require('../helpers/validators');
 
 router
-  .route("/address/create")
+  .route('/address/create')
   .post(
     [Auth, Access.verifyAccess, Access.verifyAdmin],
-    addressValidation(), validate,
+    addressValidation(),
+    validate,
     DeliveryAddressController.createAddress
   );
 
 router
-  .route("/address/view/all") //?q={state}
+  .route('/address/view/all') //?q={state}
   .get([Auth, Access.verifyAccess], DeliveryAddressController.viewAddresses);
 
 router
-  .route("/address/view/:id")
+  .route('/address/view/:id')
   .get([Auth, Access.verifyAccess], DeliveryAddressController.viewAddress);
 
 router
-  .route("/address/:id")
-  .put([Auth, Access.verifyAccess, Access.verifyAdmin], DeliveryAddressController.updateAddress)
-  .delete([Auth, Access.verifyAccess, Access.verifyAdmin], DeliveryAddressController.deleteAddress)
-
+  .route('/address/:id')
+  .put(
+    [Auth, Access.verifyAccess, Access.verifyAdmin],
+    DeliveryAddressController.updateAddress
+  )
+  .delete(
+    [Auth, Access.verifyAccess, Access.verifyAdmin],
+    DeliveryAddressController.deleteAddress
+  );
 
 module.exports = router;
